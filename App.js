f366a8681860eddc7fa7fe933288a8d310f8d54f
ochirsukhM <orange.22.ochirsukhmunkhchuluun@gmail.com>
2023-04-28 08:43:20 +0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View,Image, TouchableOpacity, Button } from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import {NavigationContainer} from "@react-navigation/native"
+
+import DetailsScreen from "./components/DetailsScreen"
+
+const Stack= createNativeStackNavigator()
+function HomeScreen({navigation}){
+  return <View style={styles.container}>
+  <View>
+  <Image 
+    style={styles.stretch}
+    source={require("./assets/logo.png")}
+  />
+  </View>
+ <View>
+ <Text style={styles.text}>SIMPLE TRANSIT</Text>
+ </View>
+  <View>
+  <TouchableOpacity
+
+  style={styles.button}
+  onPress={()=>navigation.navigate("Details")}
+  >
+    <Text style={styles.buttontext}>Get started !</Text>
+  </TouchableOpacity>
+  </View>
+
+</View>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:100,
+    marginBottom:100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
+  stretch: {
+    width: 200,
+    height: 200,
+    resizeMode: 'stretch',
+  },
+  button: {
+    width:200,
+    height:40,
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    margin:"auto",
+    borderColor: "rgb(66, 168, 249)",
+    borderStyle:'solid',
+    borderRadius:20,
+    borderWidth:1
+  },
+  buttontext:{
+    paddingLeft:5,
+    marginTop:5,
+    fontSize:20,
+    marginBottom:5,
+    color:"rgb(66, 168, 249)"
+  },
+  text:{
+    width:200,
+    fontSize:23,
+    color:"rgb(66, 168, 249)"
+  }
 });
---
+++ components/DetailsScreen.js
@@ -0,0 +1,10 @@
+
+import { View,Text } from "react-native"
+
+export default function DetailsScreen(){
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
